0249845549acbfa56ecf0badd7d7cfb485dbc1c7
Dinara <inraidi@gmail.com>
2020-11-04 18:22:50 -0500

    Added scoring system [needs collision fixing]

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public int coins;
+    public Text coinCountText;
+
+    void Start()
+    {
+        coins = 0;
+    }
+
+
+    void Update()
+    {
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log("Coins:" + coins);
+            if (other.tag == "Coin") coins++;
+            Destroy(other.gameObject);
+            coinCountText.text = "Score: " + coins;
+        }
+
+    }
+}
