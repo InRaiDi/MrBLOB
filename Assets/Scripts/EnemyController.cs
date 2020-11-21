5aa3e98b9a2b03379d1b59543fe0fdd8611a4dff
Nikita Melyanov <nmelyano@my.centennialcollege.ca>
2020-11-20 22:58:07 -0500

    Level 2: Enemy movements
    Added enemy movements for level 2. Had to make the Enemy a bit smaller for better movement pathways

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+    public bool toggleSpeedchange = true;
+    bool invokeOnce = false;
+
+
+    private int current;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector2 pos = Vector2.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody2D>().MovePosition(pos);
+        }
+        else
+        {
+
+            if (gameObject.tag == "Enemy")
+                current = (current + 1) % target.Length;
+            
+        }
+
+
+        if (toggleSpeedchange == true)
+        {
+            if (!invokeOnce)
+            {
+                StartCoroutine("cycleSpeed");
+                invokeOnce = true;
+            }
+
+        }
+        else
+        {
+            if (invokeOnce)
+            {
+                StopCoroutine("cycleSpeed");
+                invokeOnce = false;
+            }
+        }
+    }
+
+    IEnumerator cycleSpeed()
+    {
+
+        yield return new WaitForSeconds(3f);
+
+        while (toggleSpeedchange)
+        {
+            speed += 2;
+            yield return new WaitForSeconds(3f);
+            speed -= 2;
+            yield return new WaitForSeconds(3f);
+        }
+
+
+    }
+}
